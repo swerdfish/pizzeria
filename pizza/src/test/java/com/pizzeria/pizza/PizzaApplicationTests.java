49552110005d7659d075e8db5aa1c6034a7f04cf
Eric Last <eric.last@ad.infosys.com>
2020-11-02 09:23:19 -0500

    initial spring boot package

---
+++ pizza/src/test/java/com/pizzeria/pizza/PizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
