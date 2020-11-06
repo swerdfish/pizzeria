70557b8fb781651ef5fa97e37d7cfb16defa46e0
Th3Equin0x <sgruver22@gmail.com>
2020-11-06 12:17:24 -0600

    Added inner classes 'Address' and 'PaymentCard' to Customer model. Refactored model to use the above classes. Pre-existing Customer documents will need to be removed from the Customer collection.

--- Pizzeria/src/main/java/com/pizzeria/training/models/Customer.java
+++ Pizzeria/src/main/java/com/pizzeria/training/models/Customer.java
@@ -8,13 +8,218 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "customers")
 public class Customer {
+	
+	private class HomeAddress {
+		private String streetAddress;
+		private String streetAddressLine2;
+		private String city;
+		private String state;
+		private String postal;
+		
+		public HomeAddress() {
+		}
+		
+		public HomeAddress(String streetAddress, String streetAddressLine2, String city, String state, String postal) {
+			super();
+			this.streetAddress = streetAddress;
+			this.streetAddressLine2 = streetAddressLine2;
+			this.city = city;
+			this.state = state;
+			this.postal = postal;
+		}
+
+		public String getStreetAddress() {
+			return streetAddress;
+		}
+
+		public void setStreetAddress(String streetAddress) {
+			this.streetAddress = streetAddress;
+		}
+
+		public String getStreetAddressLine2() {
+			return streetAddressLine2;
+		}
+
+		public void setStreetAddressLine2(String streetAddressLine2) {
+			this.streetAddressLine2 = streetAddressLine2;
+		}
+
+		public String getCity() {
+			return city;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+		public String getState() {
+			return state;
+		}
+
+		public void setState(String state) {
+			this.state = state;
+		}
+
+		public String getPostal() {
+			return postal;
+		}
+
+		public void setPostal(String postal) {
+			this.postal = postal;
+		}
+
+		@Override
+		public String toString() {
+			return "Address [\n\tstreetAddress=" + streetAddress + "\n\tstreetAddressLine2=" + streetAddressLine2 + "\n\tcity="
+					+ city + "\n\tstate=" + state + "\n\tpostal=" + postal + "\n]";
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getEnclosingInstance().hashCode();
+			result = prime * result + ((city == null) ? 0 : city.hashCode());
+			result = prime * result + ((postal == null) ? 0 : postal.hashCode());
+			result = prime * result + ((state == null) ? 0 : state.hashCode());
+			result = prime * result + ((streetAddress == null) ? 0 : streetAddress.hashCode());
+			result = prime * result + ((streetAddressLine2 == null) ? 0 : streetAddressLine2.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			HomeAddress other = (HomeAddress) obj;
+			if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+				return false;
+			if (city == null) {
+				if (other.city != null)
+					return false;
+			} else if (!city.equals(other.city))
+				return false;
+			if (postal == null) {
+				if (other.postal != null)
+					return false;
+			} else if (!postal.equals(other.postal))
+				return false;
+			if (state == null) {
+				if (other.state != null)
+					return false;
+			} else if (!state.equals(other.state))
+				return false;
+			if (streetAddress == null) {
+				if (other.streetAddress != null)
+					return false;
+			} else if (!streetAddress.equals(other.streetAddress))
+				return false;
+			if (streetAddressLine2 == null) {
+				if (other.streetAddressLine2 != null)
+					return false;
+			} else if (!streetAddressLine2.equals(other.streetAddressLine2))
+				return false;
+			return true;
+		}
+
+		private Customer getEnclosingInstance() {
+			return Customer.this;
+		}
+	}
+	
+	private class PaymentCard {
+		// TODO Implement hashing or some other kind of security for this information 
+		private long cardNumber;
+		private String expiration;
+		private short securityCode;
+		
+		public PaymentCard(long cardNumber, String expiration, short securityCode) {
+			super();
+			this.cardNumber = cardNumber;
+			this.expiration = expiration;
+			this.securityCode = securityCode;
+		}
+
+		public long getCardNumber() {
+			return cardNumber;
+		}
+
+		public void setCardNumber(long cardNumber) {
+			this.cardNumber = cardNumber;
+		}
+
+		public String getExpiration() {
+			return expiration;
+		}
+
+		public void setExpiration(String expiration) {
+			this.expiration = expiration;
+		}
+
+		public short getSecurityCode() {
+			return securityCode;
+		}
+
+		public void setSecurityCode(short securityCode) {
+			this.securityCode = securityCode;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getEnclosingInstance().hashCode();
+			result = prime * result + (int) (cardNumber ^ (cardNumber >>> 32));
+			result = prime * result + ((expiration == null) ? 0 : expiration.hashCode());
+			result = prime * result + securityCode;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			PaymentCard other = (PaymentCard) obj;
+			if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+				return false;
+			if (cardNumber != other.cardNumber)
+				return false;
+			if (expiration == null) {
+				if (other.expiration != null)
+					return false;
+			} else if (!expiration.equals(other.expiration))
+				return false;
+			if (securityCode != other.securityCode)
+				return false;
+			return true;
+		}
+
+		private Customer getEnclosingInstance() {
+			return Customer.this;
+		}
+
+		@Override
+		public String toString() {
+			return "PaymentCard [\n\tcardNumber=" + cardNumber + "\n\texpiration=" + expiration + "\n\tsecurityCode="
+					+ securityCode + "\n]";
+		}
+	}
+	
 	@Id
 	public ObjectId _id;
 	
 	private long phoneNum;
 	private String email;
-	private String address;
-	private long creditCard;
+	private HomeAddress address;
+	private PaymentCard card;
 	private List<Pizza> favoriteOrder;
 	
 	public Customer() {}
@@ -41,26 +246,26 @@ public class Customer {
 		this.email = email;
 	}
 	
-	public String getAddress() {
+	public HomeAddress getAddress() {
 		return address;
 	}
-	public void setAddress(String address) {
+	public void setAddress(HomeAddress address) {
 		this.address = address;
 	}
 	
-	public long getCreditCard() {
-		return creditCard;
+	public PaymentCard getCard() {
+		return card;
 	}
-	public void setCreditCard(long creditCard) {
-		this.creditCard = creditCard;
+	public void setCard(PaymentCard card) {
+		this.card = card;
 	}
 	
-	public Customer(long phoneNum, String email, String address, long creditCard) {
+	public Customer(long phoneNum, String email, HomeAddress address, PaymentCard card) {
 		super();
 		this.phoneNum = phoneNum;
 		this.email = email;
 		this.address = address;
-		this.creditCard = creditCard;
+		this.card = card;
 	}
 	@Override
 	public int hashCode() {
@@ -104,7 +309,7 @@ public class Customer {
 	@Override
 	public String toString() {
 		return "Customer [_id=" + _id + ", phoneNum=" + phoneNum + ", email=" + email + ", address=" + address
-				+ ", creditCard=" + creditCard + ", favoriteOrder=" + favoriteOrder + "]";
+				+ ", creditCard=" + card + ", favoriteOrder=" + favoriteOrder + "]";
 	}
 	
 	
